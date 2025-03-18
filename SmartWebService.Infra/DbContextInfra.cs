6de91aff751e98330287dbf871517640f0731478
Ricardo Santos <ricardosantos130100@gmail.com>
2025-03-18 15:28:35 -0300

    code: Add initial commit about this project
    Signed-off-by: Ricardo Santos <ricardosantos130100@gmail.com>

---
+++ SmartWebService.Infra/DbContextInfra.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartWebService.Domain;
+
+namespace SmartWebService.Infra;
+
+public class DbContextInfra: DbContext
+{
+    public DbSet<User> Users { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Database=database_web_service;Username=postgres;Password=admin");
+    }
+}
